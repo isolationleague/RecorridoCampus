f8f18549a8fc6d36a7de120171cebd66b60c646f
HuskyDM <javialofer@hotmail.com>
2018-04-26 16:51:48 -0600

    Ya funciona el boton \^o^/

--- app/src/main/java/com/example/admlab105/recorridocampus/Tab1Fragment.java
+++ app/src/main/java/com/example/admlab105/recorridocampus/Tab1Fragment.java
@@ -1,22 +1,42 @@
 package com.example.admlab105.recorridocampus;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.service.carrier.CarrierMessagingService;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,6 +66,10 @@ public class Tab1Fragment extends Fragment implements OnMapReadyCallback {
     private GoogleMap mMap;
     SupportMapFragment mapFragment;
     int puntos = 10;
+
+    double lat = 0.0, lon = 0.0;
+    private Marker marker;
+
     public Tab1Fragment() {
 
     }
@@ -61,7 +85,18 @@ public class Tab1Fragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.tab1_fragment,container,false);
+
+
+        View view = inflater.inflate(R.layout.tab1_fragment, container, false);
+        Button btnCoor =  view.findViewById(R.id.btnCoor);
+        btnCoor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                miUbic();
+            }
+        });
+
+
         mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment == null) {
             FragmentManager fm = getFragmentManager();
@@ -82,16 +117,152 @@ public class Tab1Fragment extends Fragment implements OnMapReadyCallback {
 
         for (int i = 0; i < puntos; i++) {
             //sitios.set(i, new LatLng(-34, 151)); // Tomar coordenadas de la base
-            sitios.add(new LatLng(3*i, 3*i)); // Tomar coordenadas de la base
+            sitios.add(new LatLng(3 * i, 3 * i)); // Tomar coordenadas de la base
 
             // Título de cada marcador (Tomar nombre de cada sitio de la base)
-            mMap.addMarker(new MarkerOptions().position(sitios.get(i)).title(String.valueOf(i+1)));
+            mMap.addMarker(new MarkerOptions().position(sitios.get(i)).title(String.valueOf(i + 1)));
 
             //mMap.moveCamera(CameraUpdateFactory.newLatLng(sitios.get(i)));
         }
     }
-    public void getCurrentCoordinates(View view){
-        System.out.println("Hello");
+
+
+
+
+
+    private void miUbic() {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+          /*  ActivityCompat.requestPermissions(
+                    this,
+                    new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    999
+            );
+            return;*/
+        }
+
+
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Location location = null;
+        try {
+
+            // getting GPS status
+            boolean isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // getting network status
+            boolean isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                int i;
+                // location service disabled
+            } else {
+                // if GPS Enabled get lat/long using GPS Services
+
+                if (isGPSEnabled && !isNetworkEnabled) {
+                    android.location.LocationListener locationListener1 = new android.location.LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                        }
+
+                        @Override
+                        public void onStatusChanged(String s, int i, Bundle bundle) {
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String s) {
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String s) {
+                        }
+                    };
+                    locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener1, null);
+
+                    Log.d("GPS Enabled", "GPS Enabled");
+
+                    if (locationManager != null) {
+                        while (location == null) {
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        }
+                    }
+                }
+
+                // First get location from Network Provider
+                if (isNetworkEnabled) {
+                    if (location == null) {
+                        android.location.LocationListener locationListener2 = new android.location.LocationListener() {
+                            @Override
+                            public void onLocationChanged(Location location) {
+                            }
+
+                            @Override
+                            public void onStatusChanged(String s, int i, Bundle bundle) {
+                            }
+
+                            @Override
+                            public void onProviderEnabled(String s) {
+                            }
+
+                            @Override
+                            public void onProviderDisabled(String s) {
+                            }
+                        };
+                        locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER, locationListener2, null);
+
+                        Log.d("Network", "Network");
+
+                        if (locationManager != null) {
+                            while (location == null) {
+                                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                            }
+                        }
+                    }
+
+                }
+            }
+        } catch (Exception e) {
+            // e.printStackTrace();
+            Log.e("Error : Location",
+                    "Impossible to connect to LocationManager", e);
+        }
+
+        actualizarUbic(location);
+
     }
 
+    private void actualizarUbic(Location location) {
+        if (location != null) {
+            lat = location.getLatitude();
+            lon = location.getLongitude();
+            agregarMarcador(lat, lon);
+        }
+    }
+
+    private void agregarMarcador(double la, double lo) {
+        LatLng coord = new LatLng(la, lo);
+        CameraUpdate miUbic = CameraUpdateFactory.newLatLngZoom(coord, 20f);
+        if (marker != null) marker.remove();
+        marker = mMap.addMarker(new MarkerOptions().position(coord).title("Primera cueva")
+                /*.icon(BitmapDescriptorFactory.fromResource(R.mipmap.cueva8bit)).draggable(true)*/);
+
+        mMap.animateCamera(miUbic);
+    }
 }
+
+// https://developers.google.com/maps/documentation/android-api/location?hl=es-419
+// RECORDAR SOLICITAR AL USUARIO LOS PERMISOS DE UBICACIÓN
+//https://stackoverflow.com/questions/30253123/blue-dot-and-circle-is-not-shown-on-mylocation-using-android-fused-location-api/30255219#30255219
+
+//https://www.sitepoint.com/requesting-runtime-permissions-in-android-m-and-n/ (permisos)
+
+
+
+
