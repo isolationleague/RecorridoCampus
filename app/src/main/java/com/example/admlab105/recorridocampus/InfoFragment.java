42f7533f024aef2a34fc3560028f277318ea06aa
a96252 <jmtvlu@hotmail.com>
2018-06-28 16:44:11 -0600

    Swipe and Zoom gallery v2

--- app/src/main/java/com/example/admlab105/recorridocampus/InfoFragment.java
+++ app/src/main/java/com/example/admlab105/recorridocampus/InfoFragment.java
@@ -6,6 +6,7 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -35,7 +36,8 @@ public class InfoFragment extends Fragment {
         etiqueta = this.getArguments().getString("etiq");
         db = BaseSitiosHelper.getInstance(this.getContext());
         setRetainInstance(true);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     /**
