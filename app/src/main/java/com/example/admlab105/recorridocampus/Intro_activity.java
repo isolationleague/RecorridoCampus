3feced6e98804b0b52547d80b23f9ec991ee3683
HuskyDM <javialofer@hotmail.com>
2018-06-28 17:06:04 -0600

    Tempera y texto en la introducción

--- app/src/main/java/com/example/admlab105/recorridocampus/Intro_activity.java
+++ app/src/main/java/com/example/admlab105/recorridocampus/Intro_activity.java
@@ -31,12 +31,16 @@ import android.content.Intent;
         import android.os.Handler;
         import android.os.Bundle;
         import android.app.Activity;
+import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Intro_activity extends Activity {
 
     Button bclose;
+    TextView texto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +52,14 @@ public class Intro_activity extends Activity {
                 startActivity(i);
             }
         });
+        String txt1 = "Un paseo por el campus ";
+        String txt2 = "es una aplicación para Android, diseñada como una audio guía para ";
+        String txt3 = "descubrir el patrimonio arquitectónico y el arte público de la Ciudad Universitaria Rodrigo Facio Brenes";
+        String txt4 = ", mientras la recorre. Por medio de audios, textos y fotografías se podrá conocer más sobre los bienes culturales que resguarda la Universidad de Costa Rica.";
+        String txt5 ="La aplicación fue desarrollada en su totalidad por estudiantes del curso CI-1430 Ingeniería de software 2 de la Escuela de Ciencias de la Computación e Informática, con la colaboración de estudiantes con horas asistente, de diversas áreas, del museo+UCR. Así, el museo+UCR se compromete con la divulgación de las colecciones de la UCR y con el apoyo a la docencia.";
+        texto = (TextView) findViewById(R.id.textView2);
+        texto.setText(Html.fromHtml("<p>" + "<i>" + txt1 + "</i>" + txt2 + "<i>" + txt3 + "</i>" + txt4 + "</p>" + "<p>" +  txt5  + "</p>" + "</small>"));
+        texto.setMovementMethod(new ScrollingMovementMethod());
 
     }
 }
