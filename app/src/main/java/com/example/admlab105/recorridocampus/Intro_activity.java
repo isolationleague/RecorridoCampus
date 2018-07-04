303a1a357c1fa42f439a40ef3f39b420ccd664d9
HuskyDM <javialofer@hotmail.com>
2018-07-04 15:01:06 -0600

    Re-añade los permisos que se perdieron en el merge

--- app/src/main/java/com/example/admlab105/recorridocampus/Intro_activity.java
+++ app/src/main/java/com/example/admlab105/recorridocampus/Intro_activity.java
@@ -27,11 +27,15 @@ public class Intro_activity extends AppCompatActivity {
 
 package com.example.admlab105.recorridocampus;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.ActivityCompat;
 import android.text.Html;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
@@ -50,6 +54,7 @@ public class Intro_activity extends Activity {
     SeekBar seekBar;
     Handler handler;
     Runnable runnable;
+    static final int PERMISSIONS_REQUEST_LOCATION=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,7 +86,7 @@ public class Intro_activity extends Activity {
         texto = (TextView) findViewById(R.id.textView2);
         texto.setText(Html.fromHtml("<p>" + "<i>" + txt1 + "</i>" + txt2 + "<i>" + txt3 + "</i>" + txt4 + "</p>" + "<p>" +  txt5  + "</p>" + "</small>"));
         texto.setMovementMethod(new ScrollingMovementMethod());
-
+        almacenar();
     }
 
     /**
@@ -168,4 +173,39 @@ public class Intro_activity extends Activity {
             }
         }
     };
+
+    private void almacenar() {
+        int check = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (check == PackageManager.PERMISSION_GRANTED) {
+            //Do something
+        } else {
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1024);
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
 }
